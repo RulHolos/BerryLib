7a02f8608376eb5f12af5998382d0d435b95e84c
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2025-10-29 21:22:01 +0100

    Bounds off backgrounds

--- berrylib/backgrounds/bgs.lua
+++ berrylib/backgrounds/bgs.lua
@@ -4,6 +4,7 @@ Background = Class(Object)
 ---@param is_sc_bg boolean whether this background is for spell cards
 function Background:init(is_sc_bg)
     self.group = GROUP_GHOST
+    self.bound = false
     if is_sc_bg then
         self.layer = LAYER_BG
         self.alpha = 0
