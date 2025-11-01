fb7b698c74d00f41749af5d1146cb3d603ed390e
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2025-11-01 15:18:26 +0100

    Adjusted bullet sizes

--- game/SomeGame/root.lua
+++ game/SomeGame/root.lua
@@ -1,3 +1,5 @@
+local bullet_data = require("assets.bullets").data.bullet_data
+
 local last_cg = SceneManager.newGroup("Normal", 1, false, {}, true)
 
 local test_scene = last_cg:newScene("test_entry")
@@ -7,13 +9,12 @@ function test_scene:init()
     New(ReimuPlayer)
 
     Task.New(self, function()
-        for i = 0, INF do
-            Bullet.fire("square", Bullet.COLOR.DEEP_RED, 0, 40, 1.8, 0)
-            Bullet.fire("arrow_big", Bullet.COLOR.DEEP_RED, 0, 20, 1.8, 0)
-            Bullet.fire("gun_bullet", Bullet.COLOR.DEEP_RED, 0, 0, 1.8, 0)
-            Bullet.fire("butterfly", Bullet.COLOR.DEEP_RED, 0, -20, 1.8, 0)
-            Bullet.fire("kite", Bullet.COLOR.DEEP_RED, 0, -40, 1.8, 0)
-            Task.Wait(60)
+        for col = 1, 16 do
+            local i = 210
+            for k, v in pairs(bullet_data) do
+                Bullet.fire(k, col, -185 + (col * 15), i, 0, 0)
+                i = i - 13
+            end
         end
     end)
 end
