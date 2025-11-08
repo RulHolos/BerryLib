b11fb37c6a7b5d2476edaa43188ff55a4a9a6a25
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2025-11-08 15:24:17 +0100

    white global

--- berrylib/core/global.lua
+++ berrylib/core/global.lua
@@ -4,7 +4,6 @@ lstg.var = {}
 ---Temporary global variables
 lstg.tmpvar = {}
 
-local old_res = GetResourceStatus()
-SetResourceStatus("global")
-LoadImageFromFile("white", "general/white.png", false)
-SetResourceStatus(old_res)
+WithResourceCtx("global", function()
+    LoadImageFromFile("white", "general/white.png", false)
+end)
