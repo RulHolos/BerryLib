f2488605488f261452c1fdc4e692b756237a1063
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2025-11-11 14:25:07 +0100

    Added loaded assets

--- berrylib/assets/ui.lua
+++ berrylib/assets/ui.lua
@@ -1,9 +1,19 @@
 local M = {}
 
 M.resdesc = {
-    { name = 'ui:bg', type = 'imgfile', args = {"ui/ui_bg.png", true} },
-    { name = 'ui:lines', type = 'tex', args = {"ui/line.png", true} },
-    { name = 'ui:line_', type = 'imggrp', args = {"ui:lines", 0, 0, 200, 8, 1, 7, 0, 0} }
+    { name = "ui:bg", type = "imgfile", args = {"ui/ui_bg.png", true} },
+    { name = "ui:logo", type = "imgfile", args = {"ui/logo.png", true} },
+    { name = "ui:lines", type = "tex", args = {"ui/line.png", true} },
+    { name = "ui:line_", type = "imggrp", args = {"ui:lines", 0, 0, 200, 8, 1, 7, 0, 0} },
+    { name = "ui:hint", type = "tex", args = {"ui/hint.png", true} },
+    { name = "ui:hint:hiscore", type = "img", args = {"ui:hint", 424, 8, 80, 20} },
+    { name = "ui:hint:score", type = "img", args = {"ui:hint", 424, 30, 64, 20} },
 }
+
+function M.postLoad()
+    SetImageCenter("ui:logo", 0, 0)
+    SetImageCenter("ui:hint:hiscore", 0, 10)
+    SetImageCenter("ui:hint:score", 0, 10)
+end
 
 return M
