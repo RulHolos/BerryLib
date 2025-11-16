0952c975a1769c06ea702e68053b18c59e1964e9
Rül Hölos <60986988+RulHolos@users.noreply.github.com>
2025-11-16 11:56:14 +0100

    leaf

--- berrylib/assets/misc.lua
+++ berrylib/assets/misc.lua
@@ -3,6 +3,7 @@ local M = {}
 M.resdesc = {
     { name = 'misc', type = 'tex', args = {'ui/misc.png', true} },
     { name = "player:aura", type = "img", args = {"misc", 128, 0, 64, 64} },
+    { name = "leaf", type = "img", args = {"misc", 0, 32, 32, 32} },
 
     { name = "score3", type = "fnt", args = {"font/score_new_score.fnt", false} },
 }
